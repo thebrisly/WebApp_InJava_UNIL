84390446fcfaccad0f2347af9d4f91076949fef6
Laura hp <laura.fabbiano.gomez@hotmail.com>
2023-10-11 08:53:55 +0200

    added practice files

---
+++ Java_Practice/Reviews.java
@@ -0,0 +1,184 @@
+package ch.unil.doplab.reviewsquestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Software Architectures | DOPLab | UniL
+ *
+ * @author Melike Geçer
+ */
+public class Reviews {
+
+    public static void main(String[] args) {
+        System.out.println("---> start <---");
+        System.out.println("---> Exercise 1");
+        exercise001();
+        System.out.println("---> Exercise 2");
+        exercise002();
+        System.out.println("---> Exercise 3");
+        exercise003();
+        System.out.println("---> Exercise 4");
+        exercise004();
+        System.out.println("---> Exercise 5");
+        exercise005();
+        System.out.println("---> Exercise 6");
+        exercise006();
+        System.out.println("---> Exercise 7");
+        exercise007();
+        System.out.println("---> Exercise 8");
+        exercise008();
+        System.out.println("---> Exercise 9");
+        exercise009();
+        System.out.println("---> Exercise 10");
+        exercise010();
+        System.out.println("---> Exercise 11");
+        exercise011();
+        System.out.println("---> Exercise 12");
+        exercise012();
+        System.out.println("---> Exercise 13");
+        exercise013();
+        System.out.println("---> Exercise 14");
+        exercise014();
+        System.out.println("---> end <---");
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 1 | Printing.">
+    public static void exercise001() {
+        // TODO: Print "Hello World!".
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 2 | Explicit variables.">
+    public static void exercise002() {
+        // TODO: Define and initialize an integer explicit variables.
+        // TODO: (optional) Print it.
+        // TODO: Define and initialize a double explicit variables.
+        // TODO: (optional) Print it.
+        // TODO: Define and initialize a float explicit variables.
+        // TODO: (optional) Print it.
+        // TODO: Define and initialize a string explicit variables.
+        // TODO: (optional) Print it.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 3 | Implicit variables.">
+    public static void exercise003() {
+        // TODO: Define and initialize an integer implicit variables.
+        // TODO: (optional) Print it.
+        // TODO: Define and initialize a double implicit variables.
+        // TODO: (optional) Print it.
+        // TODO: Define and initialize a float implicit variables.
+        // TODO: (optional) Print it.
+        // TODO: Define and initialize a string implicit variables.
+        // TODO: (optional) Print it.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 4 | Type casting.">
+    public static void exercise004() {
+        // TODO: Define a double variable and initialize its value to 3.1415.
+        // TODO: (optional) Print it.
+        // TODO: Convert the double variable to a float, an integer, and a string variable.
+        // TODO: (optional) Print it.
+        // TODO: Convert the double variable to an integer variable.
+        // TODO: (optional) Print it.
+        // TODO: Convert the double variable to a string variable.
+        // TODO: (optional) Print it.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 5 | Booleans.">
+    public static void exercise005() {
+        boolean a;
+        boolean b;
+        // TODO: Initialize the boolean with false value.
+        // TODO: Initialize the boolean with true value.
+        // TODO: What will the value of the boolean c. Uncomment the two lines below to find out.
+//        boolean c = a && b;
+//        System.out.println(c);
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 6 | Conditional branching.">
+    public static boolean exercise006() {
+        int year = 2022;
+        boolean isLeap;
+        // TODO: Find if the year is a leap year using conditional branching.
+        // HINT: A leap year is exactly divisible by 4 or 400 and but not.
+        return false;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 7 | Methods.">
+    public static void exercise007() {
+        // TODO: Print the square of a number.
+    }
+
+    // TODO: Write a method called square which takes an integer and calculates the square of the integer.
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Exercise 8 | User inputs.">
+    public static void exercise008() {
+        Scanner scanner = new Scanner(System.in);
+        // TODO: Define a string, ask the user's name, and initialize the string with the user's name.
+        // TODO: Define an integer, ask the user's age, and initialize the string with the user's age.
+        // TODO: Print the following sentence using the values you got from the user.
+        // Hello, I'm Melike, and I am 28 years old.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 9 | Exceptions.">
+    public static void exercise009() {
+        // TODO: Write a try-catch block to catch an ArithmeticException.
+        // HINT: If you try to divide a number by 0, the ArithmeticException will be thrown.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 10 | Explicit Lists.">
+    public static void exercise010() {
+        // TODO: Create an explicit list and initialize it with a few elements.
+        // TODO: (optional) Print the elements in the list.
+        // TODO: Add an element to the list.
+        // TODO: (optional) Print the elements in the list.
+        // TODO: Remove the second element in the list.
+        // TODO: (optional) Print the elements in the list.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 11 | Implicit Lists.">
+    public static void exercise011() {
+        // TODO: Create an implicit list and initialize it with a few elements.
+        // TODO: (optional) Print the elements in the list.
+        // TODO: Add an element to the list.
+        // TODO: (optional) Print the elements in the list.
+        // TODO: Remove the second element in the list.
+        // TODO: (optional) Print the elements in the list.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 12 | For each loop.">
+    public static void exercise012() {
+        // TODO: Create a list and initialize it with a few elements.
+        // TODO: Use a for each loop to print elements in the list.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 13 | While loop.">
+    public static void exercise013() {
+        // TODO: Define a and initialize an integer variable.
+        // TODO: Print the digits of the integer using a while loop.
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Exercise 14 | Recursion.">
+    public static void exercise014() {
+        // TODO: Define a and initialize an integer variable.
+        // TODO: Call the recursive function.
+    }
+
+    // TODO: Write a recursive method to print the digits of the integer using recursion.
+    //</editor-fold>
+}
